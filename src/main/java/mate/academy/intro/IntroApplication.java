c3ce68c415e869b63ff89660b01d693b6ee88dbf
Serhii Nazarenko <nasarenko82@gmail.com>
2024-06-15 15:47:20 -0600

    new_prodject

--- src/main/java/mate/academy/intro/IntroApplication.java
+++ src/main/java/mate/academy/intro/IntroApplication.java
@@ -1,7 +1,6 @@
 package mate.academy.intro;
 
 import mate.academy.intro.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
