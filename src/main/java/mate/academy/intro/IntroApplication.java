f260f58dfd8ee69cd4ef501a9734b570fc8151f1
Serhii Nazarenko <nasarenko82@gmail.com>
2024-06-15 15:10:41 -0600

    new_prodject

--- src/main/java/mate/academy/intro/IntroApplication.java
+++ src/main/java/mate/academy/intro/IntroApplication.java
@@ -1,5 +1,7 @@
 package mate.academy.intro;
 
+import mate.academy.intro.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IntroApplication {
+    private BookRepository bookRepository;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(IntroApplication.class, args);
@@ -14,11 +19,6 @@ public class IntroApplication {
 
     @Bean
     public CommandLineRunner commandLineRunner() {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-
-            }
-        };
+        return null;
     }
 }
