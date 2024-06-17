eff20d6faf9dda70f8f7c5029dc2712faff1f68e
Serhii Nazarenko <nasarenko82@gmail.com>
2024-06-17 10:30:13 -0600

    completed the task

--- src/main/java/mate/academy/intro/model/Book.java
+++ src/main/java/mate/academy/intro/model/Book.java
@@ -7,21 +7,30 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "books")
-@Data
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String title;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String author;
     @Column(nullable = false, unique = true)
     private String isbn;
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
