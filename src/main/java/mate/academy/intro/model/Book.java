7918e517343318cda3bd5150fee3aebd1819a8c4
Serhii Nazarenko <nasarenko82@gmail.com>
2024-06-19 00:44:59 -0600

    corrected the task

--- src/main/java/mate/academy/intro/model/Book.java
+++ src/main/java/mate/academy/intro/model/Book.java
@@ -7,19 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "books")
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
-@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
