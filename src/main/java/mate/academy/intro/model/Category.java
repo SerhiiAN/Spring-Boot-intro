7c476fb53032d1553f8d804de1a41264046fd230
Serhii Nazarenko <nasarenko82@gmail.com>
2024-07-10 18:56:34 -0600

    saved the task

---
+++ src/main/java/mate/academy/intro/model/Category.java
@@ -0,0 +1,13 @@
+package mate.academy.intro.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
