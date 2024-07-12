5b3bba13ca648529f97fd8efb28837252d1bb9bc
Serhii Nazarenko <nasarenko82@gmail.com>
2024-07-12 09:40:57 -0600

    corrected the task

--- src/main/java/mate/academy/intro/security/JwtAuthenticationFilter.java
+++ src/main/java/mate/academy/intro/security/JwtAuthenticationFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String getToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_HEADER)) {
-            return bearerToken.substring(7);
+            return bearerToken.substring(TOKEN_HEADER.length());
         }
         return null;
     }
