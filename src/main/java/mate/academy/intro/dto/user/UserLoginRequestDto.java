db2662867dc6299b2ed68dcbca09a40094d29ef1
Serhii Nazarenko <nasarenko82@gmail.com>
2024-07-13 10:19:46 -0600

    corrected the task

--- src/main/java/mate/academy/intro/dto/user/UserLoginRequestDto.java
+++ src/main/java/mate/academy/intro/dto/user/UserLoginRequestDto.java
@@ -1,15 +1,15 @@
 package mate.academy.intro.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginRequestDto(
         @Email
         @Size(min = 8, max = 35)
-        @NotEmpty
+        @NotBlank
         String email,
-        @NotEmpty
+        @NotBlank
         @Size(min = 8, max = 35)
         String password
 ) {
