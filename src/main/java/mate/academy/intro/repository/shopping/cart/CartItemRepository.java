5988aa167f675301923004f671cbda2a7172101b
Serhii Nazarenko <nasarenko82@gmail.com>
2024-07-21 10:55:39 -0600

    corrected the task

--- src/main/java/mate/academy/intro/repository/shopping/cart/CartItemRepository.java
+++ src/main/java/mate/academy/intro/repository/shopping/cart/CartItemRepository.java
@@ -3,9 +3,7 @@ package mate.academy.intro.repository.shopping.cart;
 import mate.academy.intro.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long>,
         JpaSpecificationExecutor<CartItem> {
 }
--- src/main/java/mate/academy/intro/repository/shopping/cart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/intro/repository/shopping/cart/ShoppingCartRepository.java
@@ -3,9 +3,7 @@ package mate.academy.intro.repository.shopping.cart;
 import mate.academy.intro.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long>,
         JpaSpecificationExecutor<ShoppingCart> {
     ShoppingCart findShoppingCartByUserId(Long id);
