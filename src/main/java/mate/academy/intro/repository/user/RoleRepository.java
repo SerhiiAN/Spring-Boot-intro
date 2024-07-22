6aff281b5348b47d0709a7556a076bf6ac788d88
Serhii Nazarenko <nasarenko82@gmail.com>
2024-07-22 10:47:48 -0600

    corrected the task

--- src/main/java/mate/academy/intro/repository/user/RoleRepository.java
+++ src/main/java/mate/academy/intro/repository/user/RoleRepository.java
@@ -4,9 +4,7 @@ import java.util.Optional;
 import mate.academy.intro.model.Role;
 import mate.academy.intro.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName roleName);
 }
