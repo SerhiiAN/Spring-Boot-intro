91cf78b4fc98ca33c8cbfd95c6b56c412b085b44
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-04 18:13:05 -0600

    executed the task

--- src/test/java/mate/academy/intro/controller/CategoryControllerTest.java
+++ src/test/java/mate/academy/intro/controller/CategoryControllerTest.java
@@ -37,7 +37,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CategoryControllerTest {
+class awsCategoryControllerTest {
     protected static MockMvc mockMvc;
 
     @Autowired
