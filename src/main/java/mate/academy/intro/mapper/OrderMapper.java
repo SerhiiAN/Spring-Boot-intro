7992837888cbc842534e27afdc43d86a049e928b
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-09 22:08:11 -0600

    executed the task

--- src/main/java/mate/academy/intro/mapper/OrderMapper.java
+++ src/main/java/mate/academy/intro/mapper/OrderMapper.java
@@ -3,6 +3,7 @@ package mate.academy.intro.mapper;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import mate.academy.intro.config.MapperConfig;
 import mate.academy.intro.dto.order.OrderDto;
 import mate.academy.intro.dto.order.OrderItemDto;
@@ -12,6 +13,7 @@ import mate.academy.intro.model.OrderItem;
 import mate.academy.intro.model.ShoppingCart;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class, uses = {OrderItemMapper.class})
@@ -26,8 +28,13 @@ public interface OrderMapper {
     List<OrderDto> toOrderDtoList(List<Order> orders);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "order", ignore = true)
+    OrderItem cartItemToOrderItem(CartItem cartItem);
+
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "total", source = "cart.cartItems", qualifiedByName = "total")
-    @Mapping(target = "orderItems", source = "cart.cartItems")
+    @Mapping(target = "orderItems", source = "cart.cartItems",
+            qualifiedByName = "cartItemsToOrderItemsNoTarget")
     Order cartToOrder(ShoppingCart cart, String shippingAddress);
 
     @Named("total")
@@ -35,5 +42,19 @@ public interface OrderMapper {
         return cartItems.stream()
                 .map(i -> i.getBook().getPrice().multiply(BigDecimal.valueOf(i.getQuantity())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Named("cartItemsToOrderItemsNoTarget")
+    default Set<OrderItem> cartItemsToOrderItemsNoTarget(Set<CartItem> cartItems) {
+        return cartItems.stream()
+                .map(this::cartItemToOrderItem)
+                .collect(Collectors.toSet());
+    }
+
+    default Set<OrderItem> cartItemsToOrderItems(Set<CartItem> cartItems,
+                                                 @MappingTarget Order order) {
+        Set<OrderItem> orderItems = cartItemsToOrderItemsNoTarget(cartItems);
+        orderItems.forEach(orderItem -> orderItem.setOrder(order));
+        return orderItems;
     }
 }
