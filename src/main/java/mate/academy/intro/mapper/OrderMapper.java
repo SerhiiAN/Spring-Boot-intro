93bbe7d40d871f4d5653493cbf94dcebeb916450
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-09 23:20:56 -0600

    executed the task

--- src/main/java/mate/academy/intro/mapper/OrderMapper.java
+++ src/main/java/mate/academy/intro/mapper/OrderMapper.java
@@ -1,20 +1,13 @@
 package mate.academy.intro.mapper;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import mate.academy.intro.config.MapperConfig;
 import mate.academy.intro.dto.order.OrderDto;
-import mate.academy.intro.dto.order.OrderItemDto;
 import mate.academy.intro.model.CartItem;
 import mate.academy.intro.model.Order;
 import mate.academy.intro.model.OrderItem;
-import mate.academy.intro.model.ShoppingCart;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class, uses = {OrderItemMapper.class})
 public interface OrderMapper {
@@ -22,39 +15,9 @@ public interface OrderMapper {
     @Mapping(source = "user.id", target = "userId")
     OrderDto toOrderDto(Order order);
 
-    @Mapping(source = "book.id", target = "bookId")
-    OrderItemDto toOrderItemDto(OrderItem orderItem);
-
     List<OrderDto> toOrderDtoList(List<Order> orders);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "order", ignore = true)
     OrderItem cartItemToOrderItem(CartItem cartItem);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "total", source = "cart.cartItems", qualifiedByName = "total")
-    @Mapping(target = "orderItems", source = "cart.cartItems",
-            qualifiedByName = "cartItemsToOrderItemsNoTarget")
-    Order cartToOrder(ShoppingCart cart, String shippingAddress);
-
-    @Named("total")
-    default BigDecimal getTotal(Set<CartItem> cartItems) {
-        return cartItems.stream()
-                .map(i -> i.getBook().getPrice().multiply(BigDecimal.valueOf(i.getQuantity())))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    @Named("cartItemsToOrderItemsNoTarget")
-    default Set<OrderItem> cartItemsToOrderItemsNoTarget(Set<CartItem> cartItems) {
-        return cartItems.stream()
-                .map(this::cartItemToOrderItem)
-                .collect(Collectors.toSet());
-    }
-
-    default Set<OrderItem> cartItemsToOrderItems(Set<CartItem> cartItems,
-                                                 @MappingTarget Order order) {
-        Set<OrderItem> orderItems = cartItemsToOrderItemsNoTarget(cartItems);
-        orderItems.forEach(orderItem -> orderItem.setOrder(order));
-        return orderItems;
-    }
 }
--- src/main/java/mate/academy/intro/service/order/OrderServiceImpl.java
+++ src/main/java/mate/academy/intro/service/order/OrderServiceImpl.java
@@ -1,8 +1,11 @@
 package mate.academy.intro.service.order;
 
 import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import mate.academy.intro.dto.order.CreateOrderRequestDto;
 import mate.academy.intro.dto.order.OrderDto;
@@ -11,9 +14,11 @@ import mate.academy.intro.dto.order.UpdateOrderStatusRequestDto;
 import mate.academy.intro.exception.EntityNotFoundException;
 import mate.academy.intro.mapper.OrderItemMapper;
 import mate.academy.intro.mapper.OrderMapper;
+import mate.academy.intro.model.CartItem;
 import mate.academy.intro.model.Order;
 import mate.academy.intro.model.OrderItem;
 import mate.academy.intro.model.ShoppingCart;
+import mate.academy.intro.model.Status;
 import mate.academy.intro.repository.order.OrderItemRepository;
 import mate.academy.intro.repository.order.OrderRepository;
 import mate.academy.intro.repository.shoppingcart.ShoppingCartRepository;
@@ -36,10 +41,11 @@ public class OrderServiceImpl implements OrderService {
         if (cart.getCartItems().isEmpty()) {
             throw new EntityNotFoundException("Cart is empty for user: " + userId);
         }
-        Order order = orderMapper.cartToOrder(cart, requestDto.shippingAddress());
-        Order saveOrder = orderRepository.save(order);
+        Order order = newOrder(cart, requestDto.shippingAddress());
+        order.setOrderItems(createSetOfOrderItems(order, cart.getCartItems()));
+        OrderDto orderDto = orderMapper.toOrderDto(order);
         cart.clearCart();
-        return orderMapper.toOrderDto(saveOrder);
+        return orderDto;
     }
 
     @Transactional
@@ -76,5 +82,35 @@ public class OrderServiceImpl implements OrderService {
                 () -> new EntityNotFoundException("Order not found with id: " + id));
         order.setStatus(requestDto.status());
         return orderMapper.toOrderDto(orderRepository.save(order));
+    }
+
+    private BigDecimal countTotalOrderPrice(Set<CartItem> cartItems) {
+        return cartItems.stream()
+                .map(c -> c.getBook()
+                        .getPrice()
+                        .multiply(BigDecimal.valueOf(c.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private Order newOrder(ShoppingCart cart, String shippingAddress) {
+        Order order = new Order();
+        order.setUser(cart.getUser());
+        order.setStatus(Status.PENDING);
+        order.setOrderDate(LocalDateTime.now());
+        order.setShippingAddress(shippingAddress);
+        order.setTotal(countTotalOrderPrice(cart.getCartItems()));
+        return orderRepository.save(order);
+    }
+
+    private Set<OrderItem> createSetOfOrderItems(Order order, Set<CartItem> cartItems) {
+        Set<OrderItem> orderItems = cartItems.stream()
+                .map(c -> {
+                    OrderItem orderItem = orderMapper.cartItemToOrderItem(c);
+                    orderItem.setOrder(order);
+                    return orderItem;
+                })
+                .collect(Collectors.toSet());
+        orderItemRepository.saveAll(orderItems);
+        return orderItems;
     }
 }
